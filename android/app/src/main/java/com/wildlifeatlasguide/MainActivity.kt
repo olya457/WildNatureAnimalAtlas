451ccea3a56c5cdf1b53222ca5ee026a78f23fc7
olya <olya0665643972@gmail.com>
2026-04-26 13:31:00 +0300

    init

--- android/app/src/main/java/com/wildlifeatlasguide/MainActivity.kt
+++ android/app/src/main/java/com/wildlifeatlasguide/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.wild.nature.animal.atlas
+package com.wildlifeatlasguide
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/wildlifeatlasguide/MainApplication.kt
+++ android/app/src/main/java/com/wildlifeatlasguide/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.wild.nature.animal.atlas
+package com.wildlifeatlasguide
 
 import android.app.Application
 import com.facebook.react.PackageList
